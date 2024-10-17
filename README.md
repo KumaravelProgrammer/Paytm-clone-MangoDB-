1b6849681b961e6a3965291a0cfed51a9b60ff7c
KumaravelProgrammer <kumaravelprogrammer@gmail.com>
2024-10-17 12:31:23 +0530

    Paytm Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Build a basic version of PayTM
+
+## It is The Paytm Application using the MERN Stack
---
+++ backend/config.js
@@ -0,0 +1,6 @@
+//backend/config.js
+module.exports = {
+  JWT_SECRET: "your-jwt-secret",
+
+  // You can use Your own Secret  for the Jwt_SECRECT
+};
